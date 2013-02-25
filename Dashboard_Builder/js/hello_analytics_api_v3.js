d8a82a2b7b0a7e9701e6da580599f3791e078e66
Osama Ahmad <ooahmad@gmail.com>
2013-02-24 23:48:16 -0800

    osama

---
+++ Dashboard_Builder/js/hello_analytics_api_v3.js
@@ -0,0 +1,135 @@
+var count; 
+var index = 0; 
+var newElement = {};
+
+function makeApiCall() {
+  queryAccounts();
+}
+
+function queryAccounts() {
+
+  // Get a list of all Google Analytics accounts for this user
+  gapi.client.analytics.management.accounts.list().execute(handleAccounts);
+}
+
+
+function handleAccounts(results) {
+  if (!results.code) {
+    if (results && results.items && results.items.length) {
+
+      // Get the first Google Analytics account
+	
+	count = results.items.length; 
+      var firstAccountId = results.items[index].id;
+
+      // Query for Web Properties
+      queryWebproperties(firstAccountId);
+
+    } else {
+      console.log('No accounts found for this user.')
+    }
+  } else {
+    console.log('There was an error querying accounts: ' + results.message);
+  }
+}
+
+function queryWebproperties(accountId) {
+
+  // Get a list of all the Web Properties for the account
+  gapi.client.analytics.management.webproperties.list({'accountId': accountId}).execute(handleWebproperties);
+}
+
+function handleWebproperties(results) {
+  if (!results.code) {
+    if (results && results.items && results.items.length) {
+      // Get the first Google Analytics account
+      var firstAccountId = results.items[0].accountId;
+
+      // Get the first Web Property ID
+      var firstWebpropertyId = results.items[0].id;
+
+      // Query for Profiles
+      queryProfiles(firstAccountId, firstWebpropertyId);
+
+    } else {
+      console.log('No webproperties found for this user.');
+    }
+  } else {
+    console.log('There was an error querying webproperties: ' + results.message);
+  }
+}
+
+function queryProfiles(accountId, webpropertyId) {
+
+  // Get a list of all Profiles for the first Web Property of the first Account
+  gapi.client.analytics.management.profiles.list({
+      'accountId': accountId,
+      'webPropertyId': webpropertyId
+  }).execute(handleProfiles);
+
+}
+
+function handleProfiles(results) {
+  if (!results.code) {
+    if (results && results.items && results.items.length) {
+
+      // Get the first Profile ID
+      var firstProfileId = results.items[0].id;
+
+      // Step 3. Query the Core Reporting API
+      queryCoreReportingApi(firstProfileId);
+
+    } else {
+      console.log('No profiles found for this user.');
+    }
+  } else {
+    console.log('There was an error querying profiles: ' + results.message);
+  }
+	
+	console.log(results.items[0].id +" "+ results.items[0].name + " " + results.items[0].websiteUrl);
+		var id = results.items[0].id;
+		var name = results.items[0].name;
+		var url = results.items[0].websiteUrl;
+
+		
+	
+
+}
+
+
+function queryCoreReportingApi(profileId) {
+
+  // Use the Analytics Service Object to query the Core Reporting API
+  gapi.client.analytics.data.ga.get({
+    'ids': 'ga:' + profileId,
+    'start-date': '2012-03-03',
+    'end-date': '2012-03-03',
+    'metrics': 'ga:visits'
+  }).execute(handleCoreReportingResults);
+}
+
+function handleCoreReportingResults(results) {
+  if (results.error) {
+    console.log('There was an error querying core reporting API: ' + results.message);
+  } else {
+	
+	index ++;
+
+		if (index == count) {
+				return;
+				}
+
+    printResults(results);
+  }
+}
+
+function printResults(results) {
+  if (results.rows && results.rows.length) {
+	  queryAccounts();
+
+
+  } else {
+ 
+	
+  }
+}
